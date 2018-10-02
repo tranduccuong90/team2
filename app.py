f48a9cee2eaf628a4063fb3f92dc8b23fe4fb5e7
Nam <namphamq32@gmail.com>
2018-10-02 21:57:50 +0700

    'nam'

--- app.py
+++ app.py
@@ -15,13 +15,27 @@ def caculate():
         weight = float(form["weight"])
         height = float(form["height"])
         age = int(form["age"])
+        exercise = int(form["exercise"])
         raw_bmr= 9.99*weight + 6.25*height - 4.92*age
-        if sex == "male":
-            bmr= str(raw_bmr+5)
-            return render_template("bmr.html", bmr=bmr)
-        elif sex == "female":
-            bmr= str(raw_bmr-161) 
-            return render_template("bmr.html", bmr=bmr)
+        sex_list = ["male", "female"]
+        for sex in sex_list:
+            if sex == "male":
+                bmr= raw_bmr+5
+            elif sex == "female":
+                bmr= raw_bmr-161
+        
+        if exercise == 0:
+            need = bmr*1.2
+        elif exercise == 1:
+            need = bmr*1.375
+        elif exercise == 2:
+            need = bmr*1.55
+        elif exercise == 3:
+            need = bmr*1.725
+        elif exercise == 4: 
+            need = bmr*1.9
+        
+        return render_template("bmr.html", bmr=str(bmr), need=str(need))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- post.py
+++ post.py
@@ -2,7 +2,7 @@ from mongoengine import Document, StringField, IntField, FloatField
 
 class Post(Document):
     sex = StringField()
-    weight = FloatField()
-    height = FloatField()
-    age = IntField()
-    
+    weight = FloatField(default=None)
+    height = FloatField(default=None)
+    age = IntField(default=None)
+    exercise = StringField()
